6f552838d1339c205ba39607800d401b60259ee2
ABIDULLAH786 <abidullah.bsef18@iba-suk.edu.pk>
2022-05-15 12:24:43 -0700

    Added my Front-end CRUD project

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './componenets/form';
+
+function App() {
+  return(
+    <div className='container mt-3'>
+      <h1>Home</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
